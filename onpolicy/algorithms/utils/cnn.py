a6edb3701846ec768019b29b777e3ff3545769f5
akashvelu <akashvelu@berkeley.edu>
2021-02-27 20:17:59 -0800

    Cleanup

--- onpolicy/algorithms/utils/cnn.py
+++ onpolicy/algorithms/utils/cnn.py
@@ -1,14 +1,12 @@
-import numpy as np
-
-import torch
 import torch.nn as nn
-import torch.nn.functional as F
-
 from .util import init
+
+"""CNN Modules and utils."""
 
 class Flatten(nn.Module):
     def forward(self, x):
         return x.view(x.size(0), -1)
+
 
 class CNNLayer(nn.Module):
     def __init__(self, obs_shape, hidden_size, use_orthogonal, use_ReLU, kernel_size=3, stride=1):
@@ -26,16 +24,24 @@ class CNNLayer(nn.Module):
         input_height = obs_shape[2]
 
         self.cnn = nn.Sequential(
-            init_(nn.Conv2d(in_channels=input_channel, out_channels=hidden_size//2, kernel_size=kernel_size, stride=stride)), active_func,
+            init_(nn.Conv2d(in_channels=input_channel,
+                            out_channels=hidden_size // 2,
+                            kernel_size=kernel_size,
+                            stride=stride)
+                  ),
+            active_func,
             Flatten(),
-            init_(nn.Linear(hidden_size//2 * (input_width-kernel_size+stride) * (input_height-kernel_size+stride), hidden_size)), active_func,
+            init_(nn.Linear(hidden_size // 2 * (input_width - kernel_size + stride) * (input_height - kernel_size + stride),
+                            hidden_size)
+                  ),
+            active_func,
             init_(nn.Linear(hidden_size, hidden_size)), active_func)
 
     def forward(self, x):
         x = x / 255.0
         x = self.cnn(x)
-        
         return x
+
 
 class CNNBase(nn.Module):
     def __init__(self, args, obs_shape):
