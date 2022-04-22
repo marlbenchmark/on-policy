98c40d3112d63d21d792b638ca6e42786758c648
Chao Yu <1625360790@qq.com>
2022-04-22 14:32:13 +0800

    Update distributions.py

--- onpolicy/algorithms/utils/distributions.py
+++ onpolicy/algorithms/utils/distributions.py
@@ -33,7 +33,7 @@ class FixedNormal(torch.distributions.Normal):
     def log_probs(self, actions):
         return super().log_prob(actions).sum(-1, keepdim=True)
 
-    def entrop(self):
+    def entropy(self):
         return super.entropy().sum(-1)
 
     def mode(self):
