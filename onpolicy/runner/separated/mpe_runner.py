8501f5f02d03d518423fba77ff6d2157156d12e0
Samuele <bolottasamuele@gmail.com>
2022-07-15 14:47:57 +0200

    Update mpe_runner.py

--- onpolicy/runner/separated/mpe_runner.py
+++ onpolicy/runner/separated/mpe_runner.py
@@ -68,8 +68,8 @@ class MPERunner(Runner):
                     for agent_id in range(self.num_agents):
                         idv_rews = []
                         for info in infos:
-                            if 'individual_reward' in info[agent_id].keys():
-                                idv_rews.append(info[agent_id]['individual_reward'])
+                            if 'individual_reward' in infos[agent_id].keys():
+                                idv_rews.append(infos[agent_id]['individual_reward'])
                         train_infos[agent_id].update({'individual_rewards': np.mean(idv_rews)})
                         train_infos[agent_id].update({"average_episode_rewards": np.mean(self.buffer[agent_id].rewards) * self.episode_length})
                 self.log_train(train_infos, total_num_steps)
